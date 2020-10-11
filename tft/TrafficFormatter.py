09b0eb00350cc34cd412d3b5937ff91a76f975b4
Stephan Meyn <stephanmeyn@gmail.com>
2020-10-12 10:17:01 +1100

    runs with tf 2.0

---
+++ tft/TrafficFormatter.py
@@ -0,0 +1,171 @@
+from libs import utils        # Load TFT helper functions
+import sklearn.preprocessing  # Used for data standardization
+from data_formatters.base import GenericDataFormatter, DataTypes, InputTypes
+
+# Implement formatting functions
+
+
+class TrafficFormatter(GenericDataFormatter):
+    """Defines and formats data for the traffic dataset.
+
+    This also performs z-score normalization across the entire dataset, hence
+    re-uses most of the same functions as volatility.
+
+    Attributes:
+    column_definition: Defines input and data type of column used in the
+      experiment.
+    identifiers: Entity identifiers used in experiments.
+    """
+
+    # This defines the types used by each column
+    _column_definition = [
+        ('id', DataTypes.REAL_VALUED, InputTypes.ID),
+        ('hours_from_start', DataTypes.REAL_VALUED, InputTypes.TIME),
+        ('values', DataTypes.REAL_VALUED, InputTypes.TARGET),
+        ('time_on_day', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
+        ('day_of_week', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
+        ('hours_from_start', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
+        ('categorical_id', DataTypes.CATEGORICAL, InputTypes.STATIC_INPUT),
+    ]
+
+    def split_data(self, df, valid_boundary=151, test_boundary=166):
+        """Splits data frame into training-validation-test data frames.
+
+        This also calibrates scaling object, and transforms data for each split.
+
+        Args:
+          df: Source data frame to split.
+          valid_boundary: Starting year for validation data
+          test_boundary: Starting year for test data
+
+        Returns:
+          Tuple of transformed (train, valid, test) data.
+        """
+
+        print('Formatting train-valid-test splits.')
+
+        index = df['sensor_day']
+        train = df.loc[index < valid_boundary]
+        valid = df.loc[(index >= valid_boundary - 7) & (index < test_boundary)]
+        test = df.loc[index >= test_boundary - 7]
+
+        self.set_scalers(train)
+
+        return (self.transform_inputs(data) for data in [train, valid, test])
+
+    def set_scalers(self, df):
+        """Calibrates scalers using the data supplied.
+
+        Args:
+          df: Data to use to calibrate scalers.
+        """
+        print('Setting scalers with training data...')
+
+        column_definitions = self.get_column_definition()
+        id_column = utils.get_single_col_by_input_type(InputTypes.ID,
+                                                       column_definitions)
+        target_column = utils.get_single_col_by_input_type(InputTypes.TARGET,
+                                                           column_definitions)
+
+        # Extract identifiers in case required
+        self.identifiers = list(df[id_column].unique())
+
+        # Format real scalers
+        real_inputs = utils.extract_cols_from_data_type(
+            DataTypes.REAL_VALUED, column_definitions,
+            {InputTypes.ID, InputTypes.TIME})
+
+        data = df[real_inputs].values
+        self._real_scalers = sklearn.preprocessing.StandardScaler().fit(data)
+        self._target_scaler = sklearn.preprocessing.StandardScaler().fit(
+            df[[target_column]].values)  # used for predictions
+
+        # Format categorical scalers
+        categorical_inputs = utils.extract_cols_from_data_type(
+            DataTypes.CATEGORICAL, column_definitions,
+            {InputTypes.ID, InputTypes.TIME})
+
+        categorical_scalers = {}
+        num_classes = []
+        for col in categorical_inputs:
+            # Set all to str so that we don't have mixed integer/string columns
+            srs = df[col].apply(str)
+            categorical_scalers[col] = sklearn.preprocessing.LabelEncoder().fit(
+                srs.values)
+            num_classes.append(srs.nunique())
+
+        # Set categorical scaler outputs
+        self._cat_scalers = categorical_scalers
+        self._num_classes_per_cat_input = num_classes
+
+    def transform_inputs(self, df):
+        """Performs feature transformations.
+
+        This includes both feature engineering, preprocessing and normalisation.
+
+        Args:
+          df: Data frame to transform.
+
+        Returns:
+          Transformed data frame.
+
+        """
+        output = df.copy()
+
+        if self._real_scalers is None and self._cat_scalers is None:
+            raise ValueError('Scalers have not been set!')
+
+        column_definitions = self.get_column_definition()
+
+        real_inputs = utils.extract_cols_from_data_type(
+            DataTypes.REAL_VALUED, column_definitions,
+            {InputTypes.ID, InputTypes.TIME})
+        categorical_inputs = utils.extract_cols_from_data_type(
+            DataTypes.CATEGORICAL, column_definitions,
+            {InputTypes.ID, InputTypes.TIME})
+
+        # Format real inputs
+        output[real_inputs] = self._real_scalers.transform(
+            df[real_inputs].values)
+
+        # Format categorical inputs
+        for col in categorical_inputs:
+            string_df = df[col].apply(str)
+            output[col] = self._cat_scalers[col].transform(string_df)
+
+        return output
+
+    def format_predictions(self, predictions):
+        """Reverts any normalisation to give predictions in original scale.
+
+        Args:
+          predictions: Dataframe of model predictions.
+
+        Returns:
+          Data frame of unnormalised predictions.
+        """
+        output = predictions.copy()
+
+        column_names = predictions.columns
+
+        for col in column_names:
+            if col not in {'forecast_time', 'identifier'}:
+                output[col] = self._target_scaler.inverse_transform(
+                    predictions[col])
+
+        return output
+
+    def get_fixed_params(self):
+        """Returns fixed model parameters for experiments."""
+
+        fixed_params = {
+            'total_time_steps': 8*24,     # Total width of the Temporal Fusion Decoder
+            # Length of LSTM decoder (ie. # historical inputs)
+            'num_encoder_steps': 7*24,
+            'num_epochs': 100,            # Max number of epochs for training
+            # Early stopping threshold for # iterations with no loss improvement
+            'early_stopping_patience': 5,
+            'multiprocessing_workers': 5  # Number of multi-processing workers
+        }
+
+        return fixed_params
---
+++ tft/traffic.py
@@ -0,0 +1,150 @@
+import pandas as pd
+import os
+from script_download_data import main as download_data
+from libs.tft_model import TemporalFusionTransformer
+from data_formatters.base import GenericDataFormatter, DataTypes, InputTypes
+from libs import utils        # Load TFT helper functions
+import sklearn.preprocessing  # Used for data standardization
+from TrafficFormatter import TrafficFormatter
+import os
+import tensorflow as tf
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filenam="debuglog.txt")
+USE_GPU = True
+
+
+def download_parameters():
+    # Download parameters
+    expt_name = "traffic"
+    # Name of default experiment
+    # Root folder to save experiment outputs
+    output_folder = os.path.join(os.getcwd(), 'outputs')
+    force_download = False  # Skips download if data is already present
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Downloads main csv file if not present
+    csv_file = os.path.join(output_folder, 'data',
+                            'traffic', 'hourly_data.csv')
+    if not os.path.exists(csv_file):
+        download_data(expt_name, force_download=True,
+                      output_folder=output_folder)
+    converters = {
+       0: int, 1:float,2: float,3: float,4: int, 5:int, 6:int,7: int, 8:int, 9:float,10: int,11:int
+    }
+    # Load the downloaded data
+    df = pd.read_csv(csv_file, index_col=0, converters = converters, na_filter=False)
+    return df, output_folder
+
+
+def run_train():
+    if USE_GPU:
+        tf_config = utils.get_default_tensorflow_config(
+            tf_device="gpu", gpu_id=0)
+    else:
+        tf_config = utils.get_default_tensorflow_config(
+            tf_device="cpu", gpu_id=0)
+
+    tf.compat.v1.reset_default_graph()
+    with tf.Graph().as_default(), tf.compat.v1.Session(config=tf_config) as sess:
+
+        tf.compat.v1.keras.backend.set_session(sess)
+
+        # Create a TFT model
+        model = TemporalFusionTransformer(model_params,
+                                          use_cudnn=USE_GPU)  # Don't Run model on GPU using CuDNNLSTM cells
+
+        # Sample data into minibatches for training
+        if not model.training_data_cached():
+            model.cache_batched_data(train, "train", num_samples=450000)
+            model.cache_batched_data(valid, "valid", num_samples=50000)
+
+        # Train and save model
+        model.fit()
+        model.save(model_folder)
+    return model
+
+
+if __name__ == '__main__':
+    logging.info("Starting")
+    df, output_folder = download_parameters()
+    # Create a data formatter
+    data_formatter = TrafficFormatter()
+
+    # Split data
+
+    logging.info("Splitting data")
+    train, valid, test = data_formatter.split_data(df)
+
+    logging.info("getting experiment params")
+    data_params = data_formatter.get_experiment_params()
+
+    # Model parameters for calibration
+    model_params = {'dropout_rate': 0.3,     # Dropout discard rate
+                    'hidden_layer_size': 320,  # Internal state size of TFT
+                    'learning_rate': 0.001,   # ADAM initial learning rate
+                    'minibatch_size': 128,    # Minibatch size for training
+                    'max_gradient_norm': 100.,  # Max norm for gradient clipping
+                    'num_heads': 4,           # Number of heads for multi-head attention
+                    # Number of stacks (default 1 for interpretability)
+                    'stack_size': 1
+                    }
+
+    # Folder to save network weights during training.
+    model_folder = os.path.join(
+        output_folder, 'saved_models', 'traffic', 'fixed')
+    model_params['model_folder'] = model_folder
+
+    model_params.update(data_params)
+    # Specify GPU usage
+    if USE_GPU:
+        tf_config = utils.get_default_tensorflow_config(
+            tf_device="gpu", gpu_id=0)
+    else:
+        tf_config = utils.get_default_tensorflow_config(
+            tf_device="cpu", gpu_id=0)
+    logging.info("Starting Training")
+                
+    model = run_train()
+
+    tf.compat.v1.reset_default_graph()
+    with tf.Graph().as_default(), tf.Session(config=tf_config) as sess:
+
+        tf.keras.backend.set_session(sess)
+
+        # Create a new model & load weights
+        model = TemporalFusionTransformer(model_params,
+                                          use_cudnn=True)
+        model.load(model_folder)
+
+        # Make forecasts
+        logging.info("Starting predictions")
+    
+        output_map = model.predict(test, return_targets=True)
+
+        targets = data_formatter.format_predictions(output_map["targets"])
+
+        # Format predictions
+        p50_forecast = data_formatter.format_predictions(output_map["p50"])
+        p90_forecast = data_formatter.format_predictions(output_map["p90"])
+
+        def extract_numerical_data(data):
+            """Strips out forecast time and identifier columns."""
+            return data[[
+                col for col in data.columns
+                if col not in {"forecast_time", "identifier"}
+            ]]
+
+        # Compute Losses
+        p50_loss = utils.numpy_normalised_quantile_loss(
+            extract_numerical_data(
+                targets), extract_numerical_data(p50_forecast),
+            0.5)
+        p90_loss = utils.numpy_normalised_quantile_loss(
+            extract_numerical_data(
+                targets), extract_numerical_data(p90_forecast),
+            0.9)
+    print("Normalised quantile losses: P50={}, P90={}".format(
+        p50_loss.mean(), p90_loss.mean()))
